a23ec6ba8126d7f37218ad8a33ed25d498348cca
ID-EAST | Шляхов Сергей <89996249834@ya.ru>
2020-06-05 00:39:14 +0400

    начало конфигурации удаленного приложения

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,49 @@ var express = require('express');
 var app = express();
 
 const VkBot = require('node-vk-bot-api')
-// const bot = new VkBot(process.env.TOKEN)
+const bot = new VkBot(process.env.TOKEN)
 
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
 
-createConnection().then(async connection => {
+app.get('/', function(req, res) {
+    console.log(req);
+    console.log('/');
+    res.send('cacf322a');
+});
 
-    console.log(process.env.TOKEN);
+app.get('/confirmVK', function(req, res) {
+    console.log(req);
+    console.log('confirmVK');
+    res.send('cacf322a');
+});
+
+createConnection({
+    type: "mysql",
+    host: process.env.DB_host,
+    port: parseInt(process.env.DB_port),
+    username: process.env.DB_username,
+    password: process.env.DB_password,
+    database: process.env.DB_database,
+    entities: [
+        "src/entity/**/*.ts"
+    ],
+    migrations: [
+        "src/migration/**/*.ts"
+    ],
+    subscribers: [
+        "src/subscriber/**/*.ts"
+    ],
+    cli: {
+        "entitiesDir": "src/entity",
+        "migrationsDir": "src/migration",
+        "subscribersDir": "src/subscriber"
+    }
+}).then(async connection => {
+
+    console.log('DB Started');
+
+    // console.log(process.env.TOKEN);
 
     // console.log("Inserting a new user into the database...");
     // const user = new User();
@@ -28,7 +63,3 @@ createConnection().then(async connection => {
     // console.log("Here you can setup and run express/koa/any other framework.");
 
 }).catch(error => console.log(error));
-
-app.get('/', function(req, res) {
-    res.send('hello world');
-});
