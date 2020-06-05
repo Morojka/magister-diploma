31a9b99dd9ad3ce630dee5cab9905b3cfbb834da
ID-EAST | Шляхов Сергей <89996249834@ya.ru>
2020-06-05 22:14:35 +0400

    добавлены стили, верстка

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
-{
+module.exports = {
     "type": "mysql",
-    "host": "localhost",
-    "port": 3306,
-    "username": "studyHelper",
-    "password": "5577",
-    "database": "studyHelper",
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "synchronize": true,
     "logging": false,
     "entities": [
