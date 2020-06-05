6b86473e2d3b95b89481d8f1032ab76c4cbe5828
ID-EAST | Шляхов Сергей <89996249834@ya.ru>
2020-06-05 22:31:46 +0400

    изменен способ подключения орм

--- src/index.ts
+++ src/index.ts
@@ -8,29 +8,8 @@ if (process.env.NODE_ENV !== 'production') {
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
 
-createConnection({
-    type: "mysql",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    entities: [
-        "src/entity/**/*.ts"
-    ],
-    migrations: [
-        "src/migration/**/*.ts"
-    ],
-    subscribers: [
-        "src/subscriber/**/*.ts"
-    ],
-    cli: {
-        "entitiesDir": "src/entity",
-        "migrationsDir": "src/migration",
-        "subscribersDir": "src/subscriber"
-    }
-}).then(async connection => {
-    console.log('DB Started ' + process.env.DB_HOST);
+createConnection().then(async connection => {
+    console.log('DB Started');
 
     const users = await connection.manager.find(User);
     console.log("Loaded users: ", users);
