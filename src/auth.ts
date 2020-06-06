ead5a63c2e00c468fb7496820e6d7ac57da29ae6
ID-EAST | Шляхов Сергей <89996249834@ya.ru>
2020-06-06 17:10:00 +0400

    vk auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,41 +1,44 @@
 import "reflect-metadata";
-import {getRepository} from "typeorm";
+import {createConnection, getRepository} from "typeorm";
 import {User} from "./entity/User";
 
 var passport = require('passport')
 var VKontakteStrategy = require('passport-vkontakte').Strategy;
 
-passport.use(new VKontakteStrategy({
-        clientID: process.env.VK_APP_ID,
-        clientSecret: process.env.VK_APP_SECURE_KEY,
-        callbackURL: process.env.VK_APP_CALLBACK_URL,
-        apiVersion: '5.68',
-    },
+createConnection().then(async connection => {
+    passport.use(new VKontakteStrategy({
+            clientID: process.env.VK_APP_ID,
+            clientSecret: process.env.VK_APP_SECURE_KEY,
+            callbackURL: process.env.VK_APP_CALLBACK_URL,
+            apiVersion: '5.68',
+        },
 
-    async (_accessToken, _refreshToken, profile, done) => {
-        console.log('profile');
-        console.log(profile);
-        const userRep = getRepository(User);
-        try {
-            const user = userRep.findOne({
-                where: {
-                    vk_id: profile.id,
-                },
-            });
-            if (!user) {
-                // If the user isn't found in the database, return a message
-                return done(null, false, {message: 'User not found'});
+        async (_accessToken, _refreshToken, profile, done) => {
+            console.log('profile');
+            console.log(profile);
+            try {
+                const user = await connection.manager.findOne(User, {
+                    where: {
+                        vk_id: profile.id,
+                    },
+                });
+                if (!user) {
+                    // If the user isn't found in the database, return a message
+                    return done(null, false, {message: 'User not found'});
+                }
+                console.log('user');
+                console.log(user);
+
+                // Send the user information to the next middleware
+                return done(null, user, {message: 'Logged in Successfully'});
+            } catch (error) {
+                return done(error);
             }
-            console.log('user');
-            console.log(user);
-
-            // Send the user information to the next middleware
-            return done(null, user, {message: 'Logged in Successfully'});
-        } catch (error) {
-            return done(error);
         }
-    }
-));
+    ));
+}).catch(error => console.log(error));
+
+
 
 passport.serializeUser(function (user, done) {
     done(null, user);
